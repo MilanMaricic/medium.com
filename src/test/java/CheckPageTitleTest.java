8b11f8e1fb2a54e3ebcf7be3d03c5091efbe81a7
Milan <milanmgit@gmail.com>
2020-04-27 13:37:48 +0200

    Implement CheckPageTitleTest class for checking the title on five pages

--- src/test/java/CheckPageTitleTest.java
+++ src/test/java/CheckPageTitleTest.java
@@ -1,9 +1,15 @@
+import org.junit.experimental.theories.Theories;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Array;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,39 +20,61 @@ public class CheckPageTitleTest {
     private String projectLocation = System.getProperty("user.dir");
     private WebDriver driver;
 
+    @BeforeClass
     public void setUp(){
         //configure WebDriver which will be used for test execution
         System.setProperty("webdriver.gecko.driver", projectLocation+"\\lib\\geckodriver\\geckodriver.exe");
         System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-
     }
 
     @Test
-    public void checkrandomlyFiveTitlePages () throws InterruptedException{
+    public void checkrandomlyFiveTitlePages () throws InterruptedException {
         setUp();
         WebDriver driver = new FirefoxDriver();
         driver.get(siteURL);
 
-       WebElement menuArea = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div/div/div[3]"));
-       List<WebElement> l1 = menuArea.findElements(By.tagName("a"));
+        //List of page titles
+        String expectedTitle [] = {
+                "Medium – Get smarter about what matters to you.",
+                "Upgrade and become a member of Medium",
+                "Welcome to Medium, where words matter.",
+                "The Boundaries of Artificial Emotional Intelligence",
+                "Medium – Get smarter about what matters to you."
+        };
+        String currentTitle;
 
-       System.out.println("Number of links: "+l1.size());
+        //Compare Medium – Get smarter about what matters to you. title
+        System.out.println("Home page assert");
+        currentTitle = driver.getTitle();
+        Assert.assertEquals(currentTitle, expectedTitle[0]);
 
-       for (int i=0; i<l1.size()+2; i++){
-           Random r = new Random();
-           l1.get(r.nextInt(l1.size())).click();
-           ArrayList<String> pagesTitle = new ArrayList<String>();
-           System.out.println("Page title is: "+driver.getTitle());
-           driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        //compare Upgrade and become a member of Medium page title
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div/div/div[3]/div[1]/h4/div/a")).click();
+        System.out.println("Subscribe assert");
+        currentTitle = driver.getTitle();
+        Assert.assertEquals(currentTitle, expectedTitle[1]);
 
-           //Assert need to be implemented
+        //Compare Welcome to Medium, where words matter. page title
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div/div/div[1]/div[1]/h3/a")).click();
+        System.out.println("Our story assert");
+        currentTitle = driver.getTitle();
+        Assert.assertEquals(currentTitle, expectedTitle[2]);
 
-           driver.get(siteURL);
-           driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-           menuArea = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div/div/div[3]"));
-           l1 = menuArea.findElements(By.tagName("a"));
-       }
-       driver.quit();
+        //Compare The Boundaries of Artificial Emotional Intelligence page title
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div[2]/div/div/div/div[1]/div[2]/div[2]/div/div/div[1]/a/div")).click();
+        currentTitle = driver.getTitle();
+        Assert.assertEquals(currentTitle, expectedTitle[3]);
+
+
+
+        //Compare Medium – Get smarter about what matters to you. page title
+        driver.findElement(By.xpath("/html/body/div/div/nav/div/div[1]/div/div/div/div[1]/a")).click();
+        currentTitle = driver.getTitle();
+        Assert.assertEquals(driver.getTitle(), expectedTitle[4]);
+
+
+        driver.quit();
+
     }
 
 }
